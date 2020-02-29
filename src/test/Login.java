fd0d2235e773694994c6b5b2bf12b838a08fc9e0
taner celik <tcelikim@gmail.com>
2020-02-29 15:58:57 -0500

    =update Login.java

--- src/test/Login.java
+++ src/test/Login.java
@@ -3,7 +3,8 @@ package test;
 public class Login {
     public static void main(String[] args) {
         System.out.println("user logged in");
-        
-        // from GitHub
+
+
+        System.out.println("alert!!!"); //from intelliJ
     }
 }
